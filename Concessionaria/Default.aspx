aed86101def828d0b396991f14f35b0083959ae2
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-02-15 16:57:59 +0100

    Aggiungere i file di progetto.

---
+++ Concessionaria/Default.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Concessionaria._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+         <h1>Acquista</h1>
+    <form>
+ 
+
+ <div class="form-group mt-3">
+     <label for="DropDownList1">Seleziona Macchina e modello</label>
+     <asp:DropDownList ID="DropDownList1" runat="server"  CssClass="form-control" Width="200px">  
+     </asp:DropDownList>
+       <br />
+
+
+     <div class="row">
+
+     <label for="Checkbox1">Cambio</label>
+     <input id="Checkbox1" type="checkbox" />
+        
+
+     <label for="Checkbox2">Pilota automatico</label>
+     <input id="Checkbox2" type="checkbox" />          
+          
+
+     <div  class="row">
+     <label for="Checkbox3">Corso pronto soccorso</label>
+     <input id="Checkbox3" type="checkbox" />
+           
+
+
+      <label for="Checkbox4">Navigatore</label>
+     <input id="Checkbox4" type="checkbox" />
+              
+
+
+    <label for="Checkbox5">Cerchi in lega</label>
+    <input  id="Checkbox5" type="checkbox" />
+              
+      </div>
+    </div>
+
+     <asp:Button ID="Button1" runat="server" Text="Button" onClick="Stampa_Click"/>
+     <p id="stampa_selezione">
+
+     </p>
+
+ </div>
+
+    </form>
+       
+    </main>
+
+</asp:Content>
---
+++ Concessionaria/Default.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Concessionaria
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Concessionaria"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT Marca_modello FROM Macchina";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ListItem lst = new ListItem((string)reader["Marca_modello"]);
+                    DropDownList1.Items.Add(lst);
+
+                }
+            }
+            catch (Exception )
+            {
+
+                Response.Write("Errore");
+              
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+
+
+
+        protected void Stampa_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Concessionaria"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open(); //apriamo la connessione
+
+
+
+
+                SqlCommand cmd = new SqlCommand(); 
+                cmd.Connection = conn; 
+                cmd.CommandText = "SELECT Marca_modello,Prezzo_base FROM Macchina";
+
+
+                
+
+                SqlDataReader reader = cmd.ExecuteReader();
+               
+             
+
+               
+            }
+            catch
+            {
+               
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+
+    }
+}
